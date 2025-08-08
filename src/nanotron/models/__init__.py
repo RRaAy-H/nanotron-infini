85c52aff2189326c288a39254c5d336ceee4b386
KexuanZhang <kex.zhang@mail.utoronto.ca>
2025-08-08 12:30:29 -0400

    fix llama class import

--- src/nanotron/models/__init__.py
+++ src/nanotron/models/__init__.py
@@ -1,2 +1,6 @@
 # flake8: noqa
 from .base import DTypeInvariantTensor, NanotronModel, build_model, check_model_has_grad, init_on_device_and_dtype
+
+# Add LlamaForCausalLM as an alias for LlamaForTraining for compatibility
+from .llama import LlamaForTraining
+LlamaForCausalLM = LlamaForTraining
