acf16422c75dbf93cc220ddf2c1620d1b668d11e
KexuanZhang <kex.zhang@mail.utoronto.ca>
2025-08-12 14:40:26 -0400

    bl

---
+++ run_baseline_4gpu.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+export MASTER_ADDR=localhost
+export MASTER_PORT=29503
+export CUDA_VISIBLE_DEVICES=0,1,2,3
+
+echo "Starting 4-GPU baseline training (Standard Attention):"
+echo "MASTER_ADDR=$MASTER_ADDR"
+echo "MASTER_PORT=$MASTER_PORT" 
+echo "CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES"
+echo ""
+
+torchrun \
+    --nnodes=1 \
+    --nproc_per_node=4 \
+    --master_addr=$MASTER_ADDR \
+    --master_port=$MASTER_PORT \
+    run_baseline_train.py --config-file baseline_config.yaml
