e76a29c91b6bde72e44d4098abf756a25ea88a7e
KexuanZhang <kex.zhang@mail.utoronto.ca>
2025-08-12 14:47:30 -0400

    baseline

--- run_baseline_4gpu.sh
+++ run_baseline_4gpu.sh
@@ -4,7 +4,7 @@ export MASTER_ADDR=localhost
 export MASTER_PORT=29503
 export CUDA_VISIBLE_DEVICES=0,1,2,3
 
-echo "Starting 4-GPU baseline training (Standard Attention):"
+echo "Starting 4-GPU distributed training with fineweb data (Standard Attention):"
 echo "MASTER_ADDR=$MASTER_ADDR"
 echo "MASTER_PORT=$MASTER_PORT" 
 echo "CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES"
@@ -15,4 +15,4 @@ torchrun \
     --nproc_per_node=4 \
     --master_addr=$MASTER_ADDR \
     --master_port=$MASTER_PORT \
-    run_baseline_train.py --config-file baseline_config.yaml
+    run_train.py --config-file fineweb_local_200m_standard_4gpu_config.yaml
