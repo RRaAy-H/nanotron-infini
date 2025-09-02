a1ed953e9d3b1a6cfd589dc8659e538f9664ad9f
Kexuan Zhang <74875627+KexuanZhang@users.noreply.github.com>
2025-09-02 11:23:58 -0400

    Refactor run_evals.sh to accept depth_percent as argument
    Updated the script to accept depth_percent as an argument and modified the torchrun command with new parameters.

--- examples/infinite-context-length/scripts/run_evals.sh
+++ examples/infinite-context-length/scripts/run_evals.sh
@@ -1,8 +1,17 @@
-# Calculate depth_percent based on SLURM_ARRAY_TASK_ID
-depth_percent=$(( SLURM_ARRAY_TASK_ID * 5 ))  # 0, 5, 10, ..., 100
+#!/bin/bash
 
-# Print out the calculated depth percent for debugging/logging
-echo "Running job with depth percent: $depth_percent"
+depth_percent=${1:-0}
+
+# Print out the depth percent for debugging/logging
+echo "Running evaluation with depth percent: $depth_percent"
 
 # Run the torchrun command with the calculated depth_percent
-CUDA_DEVICE_MAX_CONNECTIONS=1 torchrun --nproc_per_node=1 examples/infinite-context-length/run_evals.py --ckpt-path /fsx/phuc/new_workspace/experiments/infini_attention_8b_llama/exp18_1b_llama2_100k_ctx_length_and_2m_bs/checkpoints/finetune_needle_checkpoints_with_needle_in_prediction/480 --context_length 32768 --depth_percent $depth_percent
+CUDA_VISIBLE_DEVICES=0 CUDA_DEVICE_MAX_CONNECTIONS=1 torchrun --nproc_per_node=1 /data1/infini-attn/baseline/nanotron-infini/examples/infinite-context-length/run_evals.py \
+    --ckpt-path /data1/infini-attn/baseline/nanotron-infini/checkpoints/fineweb_4gpu_200m_standard_test/30000 \
+    --context_length 4096 \
+    --depth_percent $depth_percent \
+    --num_shots 0 \
+    --num_digits 3 \
+    --dp 1 \
+    --pp 1 \
+    --tp 1
